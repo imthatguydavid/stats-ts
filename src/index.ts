9b0388ae27644d6c0b708e94d514ba01ca271697
David Kim <davidjjkim@gmail.com>
2025-02-04 13:42:22 -0800

    parse our csv data to be a bit more useful

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 import fs from 'fs';
 
-console.log('hello world');
+const matches = fs.readFileSync('football.csv', {
+  encoding: 'utf-8',
+}).split('\n').map((row:string) => {
+  return row.split(',')
+});
 
+console.log(matches);
